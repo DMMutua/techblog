c95946e548696c85cc372829de1c1c6c76a314f8
DMMutua <lengowriter99@gmail.com>
2024-01-22 13:37:26 +0300

    configures Flask-SQLAlchemy to set up a database within the main direcory of the app and initializes an instance for a db migration engine

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     """Hosts all the Configuration Settings for the TechBlog App
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_pretty_strong_password'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
