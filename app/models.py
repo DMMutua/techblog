e69cccf63e151f54d2fc6e2e68fd9b1af46cba37
DMMutua <lengowriter99@gmail.com>
2024-01-30 15:23:58 +0300

    adds a new Post table on the DB with a One-to-Many relationship basis with ```User```

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -5,8 +6,8 @@ from app import db
 
 
 class User(db.Model):
-    """Database Model for a Particular User of the app.
-    The model defines the `user` table to have the following;
+    """Database Model Table to store Particular Users of the app.
+    Implements the `user` table to have the following;
     id - Integer
     username - VARCHAR(64)
     email - VARCHAR(120)
@@ -19,5 +20,29 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
     def __repr__(self):
         return '<user {}>'.format(self.username)
+
+
+class Post(db.Model):
+    """Database Model Table for Blog Posts.
+    Implements a `post` table to have the following;
+     id - Posts key as primary key for this table
+     body - the Body of the Posts
+     timestamp - datetime to record created time
+     user_id - the foreign key from `user` table
+     """
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+    
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
