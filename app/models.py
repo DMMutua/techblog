bc2a1f1315b5da345d0f775b806e2a06141f6766
DMMutua <lengowriter99@gmail.com>
2024-01-30 17:07:20 +0300

    refactors the login route to validate User credentials and enables appropriate route redirection

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from flask_login import UserMixing
 
 
@@ -58,3 +58,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    """Loads a User to be tracked by a Flask's
+    User session"""
+    return db.session.get(User, int(id))
