ece7b56162fa62344c552e4c0a09fedd442c412d
DMMutua <lengowriter99@gmail.com>
2024-02-07 16:18:14 +0300

    hot fixes for logout route and base templates

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
+from urllib.parse import urlsplit
 from flask import render_template, flash, redirect, url_for, request
-from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
-from app import db
+from app import app, db
 from app.models import User
-from urllib.parse import urlsplit
+
 
 @app.route('/')
 @app.route('/index')
@@ -24,7 +24,7 @@ def index():
             'body': 'James Cameron`s Avatar is the Ishh'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -41,7 +41,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or urlsplit(next_page).netlock != '':
+        if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next)
     return render_template('login.html', title='Sign In', form=form)
